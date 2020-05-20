cb530fcdcc893d24d69daa0bd56b82e08b3d3907
drzh <zhangheqd@gmail.com>
2020-05-20 00:55:36 -0500

    200520

--- list.php
+++ list.php
@@ -8,14 +8,23 @@ if (isset($_GET['kw'])) {
 }
 
 // Process key word
+$type = 'text';
 if (preg_match('/(\S+):(\S+)/', $kw, $mat)) {
   $chr = $mat[0];
+  $pos = $mat[1];
   if (preg_match('/chr(\S+)/', $chr, $matchr)) {
     $chr = $matchr[1];
   }
-  
-print_r($mat);
-
+  if (preg_match('/(\d+):(\d+)/', $pos, $matpos)) {
+    $type = 'region';
+    $pos1 = $matpos[0];
+    $pos2 = $matpos[1];
+  }
+  else if (preg_match('/^(\d+)$/', $pos)) {
+    $type = 'pos';
+  }
+}
+echo 'Type: ', $type;
 ?>
 
 <?php include('tail.inc'); ?>
