6b48d7e0edc599a6650ab4eb1f735ef3de0303e5
drzh <zhangheqd@gmail.com>
2020-05-20 09:21:01 -0500

    200520

--- list.php
+++ list.php
@@ -26,52 +26,68 @@ if (preg_match('/(\S+):(\S+)/', $kw, $mat)) {
     $type = 'pos';
   }
 }
-else if (preg_match('/^ENST/', $kw, $mat)) {
+else if (preg_match('/^(ENST\d+)(\.\d+)*$/', $kw, $mat)) {
   // tid or tid_m;
-  if (preg_match('/^ENST\d+$/', $kw)) {
-    $type = 'tid_m';
-    $id_m = $kw;
-  }
-  else if (preg_match('/^(ENST\d+)\.\d+$/', $kw, $matid)) {
-    $type = 'tid';
-    $id = $kw;
-    $id_m = $matid[1];
-  }
+  $type = 'tid_m';
+  $id_m = $mat[1];
 }
-else if (preg_match('/^ENSG/', $kw, $mat)) {
+else if (preg_match('/^(ENSG\d+)(\.\d+)*$/', $kw, $mat)) {
   // gid or gid_m;
-  if (preg_match('/^ENSG\d+$/', $kw)) {
-    $type = 'gid_m';
-    $id_m = $kw;
-  }
-  else if (preg_match('/^(ENSG\d+)\.\d+$/', $kw, $matid)) {
-    $type = 'gid';
-    $id = $kw;
-    $id_m = $matid[1];
-  }
+  $type = 'gid_m';
+  $id_m = $mat[1];
 }
-else if (preg_match('/^uc\d{3}[a-z]{3}/', $kw, $mat)) {
-  // gid or gid_m;
-  if (preg_match('/^uc\d{3}[a-z]{3}$/', $kw)) {
-    $type = 'ucsc_id_m';
-    $id_m = $kw;
-    echo $type, ' | ', $id_m;
-  }
-  else if (preg_match('/^(uc\d{3}[a-z]{3})\.\d+$/', $kw, $matid)) {
-    $type = 'ucsc_id';
-    $id = $kw;
-    $id_m = $matid[1];
-    echo $type, ' | ', $id, ' | ', $id_m;
-  }
+else if (preg_match('/^(uc\d{3}[a-z]{3})(\.\d+)*$/', $kw, $mat)) {
+  // ucsc_id or ucsc_id_m;
+  $type = 'ucsc_id_m';
+  $id_m = $mat[1];
 }
 else if (preg_match('/^(N[MR]_\d{6})(\.\d+)*$/', $kw, $mat)) {
   // refid;
   $type = 'refseq_id';
   $id_m = $mat[1];
-  echo $type, ' | ', $id_m;
 }
 
+// generate sql
+$col = 'nsc.chr, nsc.pos, nsc.ref, nsc.alt, nsc.str, nsc.tid, nsc.t_pos, nsc.t_ref, nsc.t_alt, nsc.frame, nsc.end_before, nsc.nsc_start, nsc.nsc_end, gene.gid, gene.gname, gene.symbol';
+if ($type == 'pos') {
+  $sql = "select $col from nsc, gene where nsc.tid = gene.tid and nsc.chr = '$chr' and nsc.pos = $pos;";
+}
+else if ($type == 'region') {
+  $sql = "select $col from nsc, gene where nsc.tid = gene.tid and nsc.chr = '$chr' and nsc.pos >= $pos1 and nsc.pos <= $pos2;";
+}
+else if ($type == 'tid_m' || $type == 'gid_m' || $type == 'ucsc_id_m' || $type == 'refseq_id') {
+  $sql = "select $col from nsc, gene where nsc.tid = gene.tid and gene.$type = '$id_m';";
+}
 
+// query 
+echo $sql, '<br>';
+$res = $conn -> query($sql);
+if ($res -> num_rows > 0) {
+  // output
+  echo "<table class='tf_table'>";
+  echo "<thead><tr><th>Chr</th><th>Position</th><th>Ref</th><th>Alt</th><th>Transcript_ID</th><th>Position<br>in transcript</th><th>Frame</th><th>Gene Name</th><th>Symbol</th></tr></thead>";
+  echo "<tbody>";
+  while($row = $res -> fetch_assoc()) {
+    #echo $row['chr'], ' ', $row['pos'], ' ', $row['ref'], ' ', $row['alt'], ' ',  $row['str'], ' ', $row['tid'], '<br>';
+    echo "<tr id='listrow' onclick=", '"', "document.location = 'nsc.php?chr=", $row['chr'], "&pos=", $row['pos'], "&ref=", $row['ref'], "&alt=", $row['alt'], "&tid=", $row['tid'], "';", '">';
+    echo "<td>", $row['chr'], "</td>";
+    echo "<td>", $row['pos'], "</td>";
+    echo "<td>", $row['ref'], "</td>";
+    echo "<td>", $row['alt'], "</td>";
+    echo "<td>", $row['tid'], "</td>";
+    echo "<td>", $row['t_pos'], "</td>";
+    echo "<td>", $row['frame'], "</td>";
+    /* echo "<td>", $row['gid'], "</td>"; */
+    echo "<td>", $row['gname'], "</td>";
+    echo "<td>", $row['symbol'], "</td>";
+    echo "</tr>";
+  }
+  echo "</tbody>";
+  echo "</table>";
+}
+else {
+  echo "0 results";
+}
 
 ?>
 
