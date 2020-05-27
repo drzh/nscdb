e9c4c8a0547cd359ed3ef84a3f866b227996ae4c
drzh <zhangheqd@gmail.com>
2020-05-27 00:13:46 -0500

    200526

--- down.php
+++ down.php
@@ -6,25 +6,44 @@
 $target_dir = "upload/";
 $dt = date("ymdHis");
 $rand = mt_rand(100000000, 999999999);
-$fname = $dt . "." . $rand . ".in";
-$fout = $dt . "." . $rand . ".tsv";
-$target_file = $target_dir . $fname;
-$target_output = $target_dir . $fout;
+/* $fname = $dt . "." . $rand . ".in";
+ * $fout = $dt . "." . $rand . ".tsv";
+ * $target_file = $target_dir . $fname;
+ * $target_output = $target_dir . $fout; */
 $flag = 0;
 
 // check if file was uploaded succesfully and moved to the target directory
 if (isset($_FILES['upfile']['tmp_name']) && strlen($_FILES['upfile']['tmp_name']) > 1) {
-  if (move_uploaded_file($_FILES['upfile']['tmp_name'], $target_file)) {
-    $flag = 1;
+  $md5 = md5_file($_FILES['upfile']['tmp_name']);
+  $pattern = $target_dir . "*$md5.tsv";
+  $fouts = glob($pattern);
+  if ($fouts) {
+    $target_output = $fouts[0];
+    $fid = 'N.A.';
+    if (preg_match('/(\d{12}\S{32}).tsv/', $target_output, $mat)) {
+      $fid = $mat[1];
+    }
+    $flag = 2;
+  }
+  else {
+    $fid = $dt . $md5;
+    $target_file = $target_dir . $fid . ".in";
+    $target_output = $target_dir . $fid . ".tsv";
+    if (move_uploaded_file($_FILES['upfile']['tmp_name'], $target_file)) {
+      $flag = 1;
+    }
   }
 }
 
 // 
-if ($flag == 1) {
-  $cmd = "data/extract_nsc.sh data/nsc.all.gz $target_file >$target_output 2>$target_output.err ";
-  exec($cmd);
+if ($flag != 0) {
+  if ($flag == 1) {
+    $cmd = "data/extract_nsc.sh data/nsc.all.gz $target_file >$target_output 2>$target_output.err ";
+    exec($cmd);
+  }
   if (file_exists($target_output)) {
-    echo "<div align='center'><a href='$target_output'>Download the result</a></div>";
+    echo "<div align='center' style='font-size:1.5em;padding-bottom:10px;'><a href='$target_output'>Download the result</a></div>";
+    echo "<div align='center'>Result ID (result can be retrieved in future using this ID):&nbsp;&nbsp;<span style='color:red;'>$fid</span></a></div>";
   }
 }
 else {
--- up.php
+++ up.php
@@ -15,7 +15,10 @@
   <div align="center">
     <label for="myfile">Select a file:</label>
     <input type="file" id="upfile" name="upfile">
-    <input class="submit1" type="submit" name="submit" value="Upload" onclick="show_loader(); this.disabled=true;"><br>
+    <input class="submit1" type="submit" name="submit1" value="Upload" onclick="show_loader(); this.disabled=true;">
+    <div style="padding:100px,100px,100px,100px;">Or</div>
+    <label for="myfid">Input the result ID:</label>
+    <input type="text" id="fid" name="fid" size="50">
   </div>
 </form>
 
